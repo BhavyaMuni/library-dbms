e8a47e3bae9d1abc7969f5973d4f20fba766afe4
Bhavya Muni <bhavyamuni@outlook.com>
2023-11-17 19:24:28 -0500

    fix: fixed fetch url

--- frontend/src/app/books/page.tsx
+++ frontend/src/app/books/page.tsx
@@ -1,9 +1,13 @@
 import { Book, columns } from "./columns";
 import { DataTable } from "./data-table";
 
+const URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
+  : "http://localhost:3000/api";
+
 async function getBooks(): Promise<Book[]> {
   // Fetch data from your API here.
-  const res = await fetch("/api/books");
+  const res = await fetch(`${URL}/books`);
   const data = await res.json();
   return data;
   return [
