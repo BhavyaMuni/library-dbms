a1b8d4e8d49a8360a9362254ec1f20009b31142b
Bhavya Muni <bhavyamuni@outlook.com>
2023-11-17 19:34:58 -0500

    fix: fixed prerender error

--- frontend/src/app/books/page.tsx
+++ frontend/src/app/books/page.tsx
@@ -3,16 +3,13 @@ import { DataTable } from "./data-table";
 
 const URL = process.env.NEXT_PUBLIC_VERCEL_URL
   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
-  : "http://localhost:3000/api";
+  : "http://localhost:8000/api";
 
 async function getBooks(): Promise<Book[]> {
   // Fetch data from your API here.
   const res = await fetch(`${URL}/books`);
   const data = await res.json();
   return data;
-  return [
-    // ...
-  ];
 }
 
 export default async function DemoPage() {
